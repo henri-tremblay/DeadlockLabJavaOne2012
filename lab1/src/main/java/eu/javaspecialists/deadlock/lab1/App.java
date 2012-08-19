a1517138bb6e611bf6fe896adfb2b05b1c0bfb57
Henri Tremblay <henri.tremblay@gmail.com>
2012-08-19 22:23:58 +0200

    Initialize the shell of the first lab

---
+++ lab1/src/main/java/eu/javaspecialists/deadlock/lab1/App.java
@@ -0,0 +1,13 @@
+package eu.javaspecialists.deadlock.lab1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
