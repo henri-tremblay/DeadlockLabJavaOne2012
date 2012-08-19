55ca62dac514ce1536f92171c5f9e02fe74a85fa
Henri Tremblay <henri.tremblay@gmail.com>
2012-08-20 01:06:13 +0200

    First deadlock

--- lab1/src/main/java/eu/javaspecialists/deadlock/lab1/App.java
+++ lab1/src/main/java/eu/javaspecialists/deadlock/lab1/App.java
@@ -1,13 +1,47 @@
 package eu.javaspecialists.deadlock.lab1;
 
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App implements Runnable
 {
-    public static void main( String[] args )
+    private static Object lock = new Object();
+
+    private Set<Integer> numbers = new ConcurrentSkipListSet<>();
+
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        App app = new App();
+        for (int i = 0; i < 20; i++) {
+            Thread t = new Thread(app);
+            t.start();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (numbers.size() < 32000) {
+            f();
+            g();
+        }
+    }
+
+    public synchronized void f() {
+        synchronized (lock) {
+            int i = ThreadLocalRandom.current().nextInt();
+            System.out.println(i);
+            numbers.add(i);
+        }
+    }
+
+    public void g() {
+        synchronized (lock) {
+            f();
+        }
     }
 }
