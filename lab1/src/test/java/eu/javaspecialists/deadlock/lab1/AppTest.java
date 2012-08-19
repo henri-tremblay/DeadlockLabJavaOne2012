b58d08f31d6266b135050a16567883540768a138
Henri Tremblay <henri.tremblay@gmail.com>
2012-08-20 01:15:42 +0200

    Add a test that works perfectly and never show the deadlock

--- lab1/src/test/java/eu/javaspecialists/deadlock/lab1/AppTest.java
+++ lab1/src/test/java/eu/javaspecialists/deadlock/lab1/AppTest.java
@@ -1,38 +1,28 @@
 package eu.javaspecialists.deadlock.lab1;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
+    private App app = new App();
+
+    @Test
+    public void testF()
     {
-        super( testName );
+        app.f();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testG() {
+        app.g();
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testRun() {
+        app.run();
     }
 }
