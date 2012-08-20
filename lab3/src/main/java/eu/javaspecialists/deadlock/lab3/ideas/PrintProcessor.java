e0efb77c6f31bfafdd75ab55b884378d119d9a75
kabutz <heinz@javaspecialists.eu>
2012-08-20 20:56:56 +0300

    Added Walking Collection which causes a deadlock with a single thread when we try to upgrade a read lock to a write lock.

---
+++ lab3/src/main/java/eu/javaspecialists/deadlock/lab3/ideas/PrintProcessor.java
@@ -0,0 +1,11 @@
+package eu.javaspecialists.deadlock.lab3.ideas;
+
+/**
+ * DO NOT CHANGE.
+ */
+public class PrintProcessor implements Processor<Object> {
+  public boolean process(Object o) {
+    System.out.println(">>> " + o);
+    return true;
+  }
+}
